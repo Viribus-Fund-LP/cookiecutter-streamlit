f8f4a134e003727d98b0f253a810231c10ed756f
Nick Shaffer <nick@viribusfund.com>
2024-07-26 11:14:40 -0500

    Adding deps, snowflake code and streamlit basics

---
+++ {{cookiecutter.project_name}}/app.py
@@ -0,0 +1,38 @@
+import os
+
+import pandas as pd
+import streamlit as st
+
+### These need to specifically be here
+st.set_page_config(layout="wide")
+for k, v in st.session_state.items():
+    st.session_state[k] = v
+
+state_defaults = {"_sb_name_value": None, "_sb_ye_name_value": None}
+for k, v in state_defaults.items():
+    if k not in st.session_state:
+        st.session_state[k] = v
+        if k[:1] == "_":
+            st.session_state[k[1:]] = v
+for k, v in st.session_state.items():
+    st.session_state[k] = v
+###
+
+from streamlit.logger import get_logger  # noqa: E402
+
+logger = get_logger(__name__)
+
+
+def is_local():
+    return os.environ.get("LOCAL", "false").lower() == "true"
+
+
+def main():
+    st.title("Hello world!")
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        st.error(e, icon="🚨")
---
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/sf.py
@@ -0,0 +1,140 @@
+import re
+from urllib.parse import quote, quote_plus
+from uuid import uuid4
+
+import pandas as pd
+import snowflake.connector
+from snowflake.connector.compat import IS_STR
+from snowflake.connector.pandas_tools import write_pandas
+
+
+def _rfc_1738_quote(text):
+    return re.sub(r"[:@/]", lambda m: "%%%X" % ord(m.group(0)), text)
+
+
+def _quote_password(text):
+    return quote(_rfc_1738_quote(text))
+
+
+def temp_table_name(prefix="tmp", suffix="table"):
+    parts = [prefix, str(uuid4()).replace("-", ""), suffix]
+    return "_".join([str(x) for x in parts if x]).upper()
+
+
+def URL(**db_parameters):
+    specified_parameters = []
+    if "account" not in db_parameters:
+        raise Exception("account parameter must be specified.")
+    if "host" in db_parameters:
+        ret = "snowflake://{user}:{password}@{host}:{port}/".format(
+            user=db_parameters.get("user", ""),
+            password=_quote_password(db_parameters.get("password", "")),
+            host=db_parameters["host"],
+            port=db_parameters["port"] if "port" in db_parameters else 443,
+        )
+        specified_parameters += ["user", "password", "host", "port"]
+    elif "region" not in db_parameters:
+        ret = "snowflake://{user}:{password}@{account}/".format(
+            account=db_parameters["account"],
+            user=db_parameters.get("user", ""),
+            password=_quote_password(db_parameters.get("password", "")),
+        )
+        specified_parameters += ["user", "password", "account"]
+    else:
+        ret = "snowflake://{user}:{password}@{account}.{region}/".format(
+            account=db_parameters["account"],
+            user=db_parameters.get("user", ""),
+            password=_quote_password(db_parameters.get("password", "")),
+            region=db_parameters["region"],
+        )
+        specified_parameters += ["user", "password", "account", "region"]
+    if "database" in db_parameters:
+        ret += quote_plus(db_parameters["database"])
+        specified_parameters += ["database"]
+        if "schema" in db_parameters:
+            ret += "/" + quote_plus(db_parameters["schema"])
+            specified_parameters += ["schema"]
+    elif "schema" in db_parameters:
+        raise Exception("schema cannot be specified without database")
+
+    def sep(is_first_parameter):
+        return "?" if is_first_parameter else "&"
+
+    is_first_parameter = True
+    for p in sorted(db_parameters.keys()):
+        v = db_parameters[p]
+        if p not in specified_parameters:
+            encoded_value = quote_plus(v) if IS_STR(v) else str(v)
+            ret += sep(is_first_parameter) + p + "=" + encoded_value
+            is_first_parameter = False
+    return ret
+
+
+def connect(**kwargs):
+    snowflake.connector.paramstyle = "numeric"
+    return snowflake.connector.connect(**kwargs, client_session_keep_alive=True)
+
+
+class Snowflake:
+    def __init__(self, **kwargs):
+        # TODO add method to refresh and/or keep alive
+        self.conn = connect(**kwargs)
+
+    def query(self, sql, **kwargs):
+        cur = self.conn.cursor()
+        cur.execute(sql, **kwargs)
+        df = cur.fetch_pandas_all()
+        df.columns = df.columns.map(str.lower)
+        return df
+
+    def execute(self, sql, **kwargs):
+        return self.conn.cursor().execute(sql, **kwargs)
+
+    def write_pandas(self, df, table_name, quote_identifiers=False, index=False, **kwargs):
+        _df = df.rename(columns=lambda x: x.upper())
+        if index:
+            raise NotImplementedError("Passing index=True is not supported")
+
+        if not (isinstance(_df.index, pd.RangeIndex) and 1 == _df.index.step and 0 == _df.index.start):
+            if index:
+                raise Exception("index=True and index is not RangeIndex, if you want to keep index call reset_index()")
+            else:
+                _df = _df.reset_index(drop=True)
+                _df.index = pd.RangeIndex(0, len(_df.index), 1)
+
+        return write_pandas(
+            conn=self.conn,
+            df=_df,
+            table_name=table_name.upper(),
+            quote_identifiers=quote_identifiers,
+            index=index,
+            **kwargs,
+        )
+
+    def portfolio_to_temp_table(self, df):
+        # TODO genericize this and put it somewhere else
+        tbl = temp_table_name().upper()
+        self.execute(
+            """
+        create or replace temp table identifier(:1) (
+            iso varchar,
+            ftrid int,
+            startdate date,
+            term int,
+            peaktype varchar,
+            hedgetype varchar,
+            tradetype varchar,
+            sourceid int,
+            sinkid int,
+            mw number(38,1),
+            bid number(38,2),
+            meta object
+        )
+        """,
+            params=[tbl],
+        ).fetchall()
+
+        res = self.write_pandas(df=df, table_name=tbl, index=False)
+        assert res[0], "Write failed"
+
+        return tbl, res
